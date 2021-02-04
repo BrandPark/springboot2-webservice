652bd7c5c9578193c3d50f89c499bda07bcb6222
BrandPark <pak2005@hanmail.net>
2021-02-05 00:52:11 +0900

    Posts 등록, 수정, 조회 API 생성 및 테스트 코드 작성.

--- src/main/java/com/brandpark/springboot/service/posts/PostsService.java
+++ src/main/java/com/brandpark/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.brandpark.springboot.service.posts;
 
+import com.brandpark.springboot.domain.posts.Posts;
 import com.brandpark.springboot.domain.posts.PostsRepository;
+import com.brandpark.springboot.web.dto.PostsResponseDto;
 import com.brandpark.springboot.web.dto.PostsSaveRequestDto;
+import com.brandpark.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +13,25 @@ import javax.transaction.Transactional;
 @RequiredArgsConstructor
 @Service
 public class PostsService {
-    private PostsRepository postsRepository;
+    private final PostsRepository postsRepository;
 
     @Transactional
     public Long save(PostsSaveRequestDto postsSaveRequestDto) {
         return postsRepository.save(postsSaveRequestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
     }
 }
--- src/main/java/com/brandpark/springboot/web/PostsApiController.java
+++ src/main/java/com/brandpark/springboot/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.brandpark.springboot.web;
 
 import com.brandpark.springboot.service.posts.PostsService;
+import com.brandpark.springboot.web.dto.PostsResponseDto;
 import com.brandpark.springboot.web.dto.PostsSaveRequestDto;
+import com.brandpark.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,5 +15,15 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto postsSaveRequestDto){
         return postsService.save(postsSaveRequestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id,requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
     }
 }

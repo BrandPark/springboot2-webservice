588ccedcb2faad8987c2cf347a5033ffb850101f
BrandPark <pak2005@hanmail.net>
2021-02-07 21:36:47 +0900

    등록버튼에 ajax를 사용하여 등록기능 달기

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,12 +1,12 @@
-var main = {
-    init : function(){
+var index = {
+    init : function(){      //btn-save라는 css클래스를 가진 컴포넌트에 click리스너를 달아 놓는다.
         var _this = this;
         $('#btn-save').on('click', function(){
-            _this.save();
+            _this.save();       //클릭하면 save()호출
         });
     },
     save : function(){
-        var data = {
+        var data = {        //컴포넌트들의 값들을 동적으로 가져와 data라는 객체를 만든다.
             title: $('#title').val(),
             author: $('#author').val(),
             content: $('#content').val()
@@ -16,7 +16,7 @@ var main = {
             type: 'POST',
             url: '/api/v1/posts',
             dataType: 'json',
-            contentType: 'application/json: charset=utf-8',
+            contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function(){
             alert('글이 등록되었습니다.');
@@ -25,4 +25,5 @@ var main = {
             alert(JSON.stringify(error));
         })
     }
-}
+};
+index.init();

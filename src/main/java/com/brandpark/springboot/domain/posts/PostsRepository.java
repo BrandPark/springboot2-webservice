902fb3e4ab82c8cadd97cd6f085bc75b98d683ab
BrandPark <pak2005@hanmail.net>
2021-02-08 22:43:53 +0900

    메인화면에 등록된 Posts전체 조회내용을 Table로 출력

--- src/main/java/com/brandpark/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/brandpark/springboot/domain/posts/PostsRepository.java
@@ -1,11 +1,15 @@
 package com.brandpark.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /*
 * 단순히 인터페이스를 생성 후 상속만 하면 기본적인 CRUD메소드가 자동으로 생성된다.
 */
 public interface PostsRepository extends JpaRepository<Posts, Long> {   //<Entity클래스, PK타입>
-
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 
 }

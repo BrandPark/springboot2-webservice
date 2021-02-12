a586bc8cbb24b0cd85d32ab9475aca19dddab1a9
BrandPark <pak2005@hanmail.net>
2021-02-12 10:58:33 +0900

    SpringSecurity기능 추가작업 1

--- src/main/java/com/brandpark/springboot/web/PageController.java
+++ src/main/java/com/brandpark/springboot/web/PageController.java
@@ -16,6 +16,7 @@ public class PageController {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("Posts", postsService.findAllDesc());
+
         return "index";
     }
 
